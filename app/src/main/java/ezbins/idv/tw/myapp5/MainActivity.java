f754c881899388699354891e4601c741b3ec3a80
ShaoBin <ty3627@gmail.com>
2015-09-09 13:32:04 +0800

    add actionbar

--- app/src/main/java/ezbins/idv/tw/myapp5/MainActivity.java
+++ app/src/main/java/ezbins/idv/tw/myapp5/MainActivity.java
@@ -1,13 +1,23 @@
 package ezbins.idv.tw.myapp5;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
+    Fragment frag;
+    FragmentTransaction frgTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +28,47 @@ public class MainActivity extends FragmentActivity {
                     .add(R.id.menubar,new MultiFragment())
                     .commit();
         }
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.koala_btn:
+                frag = new koalaFragment();
+                frgTransaction = getFragmentManager().beginTransaction()
+                        .replace(R.id.showcontent,frag)
+                        .addToBackStack(null);
+                frgTransaction.commit();
+                return true;
+
+            case R.id.penguin_btn:
+                 frag = new penguinFragment();
+                 frgTransaction = getFragmentManager().beginTransaction()
+                         .replace(R.id.showcontent, frag)
+                         .addToBackStack(null);
+                 frgTransaction.commit();
+                 return true;
+
+            case R.id.other_btn:
+                frag = new otherFragment();
+                frgTransaction = getFragmentManager().beginTransaction()
+                        .replace(R.id.showcontent,frag)
+                        .addToBackStack(null);
+                frgTransaction.commit();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- app/src/main/java/ezbins/idv/tw/myapp5/MultiFragment.java
+++ app/src/main/java/ezbins/idv/tw/myapp5/MultiFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,51 +24,22 @@ public class MultiFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.multifragment,container,false);
-
         frag = new koalaFragment();
         frgTransaction = getFragmentManager().beginTransaction()
                 .add(R.id.showcontent,frag);
         frgTransaction.commit();
 
-        Button kola = (Button) view.findViewById(R.id.koala);
-        Button penguin = (Button) view.findViewById(R.id.penguin);
-        Button other = (Button) view.findViewById(R.id.other);
-
-        kola.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                frag = new koalaFragment();
-                frgTransaction = getFragmentManager().beginTransaction()
-                        .replace(R.id.showcontent, frag)
-                        .addToBackStack(null);
-                frgTransaction.commit();
-            }
-        });
-
-        penguin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                frag = new penguinFragment();
-                frgTransaction = getFragmentManager().beginTransaction()
-                        .replace(R.id.showcontent,frag)
-                        .addToBackStack(null);
-                frgTransaction.commit();
-            }
-        });
-
-        other.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                frag = new otherFragment();
-                frgTransaction = getFragmentManager().beginTransaction()
-                        .replace(R.id.showcontent,frag)
-                        .addToBackStack(null);
-                frgTransaction.commit();
-            }
-        });
         return view;
     }
+
 }
